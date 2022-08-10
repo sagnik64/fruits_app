5a017c1ef91933efcc02bf5371e6a5507d7c4da4
Sagnik Mandal <Sagnik@shorthillstech.com>
2022-08-10 14:40:25 +0530

    added custom routes and scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\SendTestMailJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -16,6 +17,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->job(new SendTestMailJob("Jim"))->everyMinute();
+
+        $schedule->command("queue:work --stop-when-empty")->everyMinute();
     }
 
     /**
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,6 +46,15 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+            Route::prefix('admin')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/admin.php'));
+            
+            Route::prefix('manager')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/manager.php'));    
+            
         });
     }
 
